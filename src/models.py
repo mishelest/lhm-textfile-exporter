22994f2bbe2a7cd870eb300e088845735341adea
Mishel Estrin <mishelest@gmail.com>
2026-08-11 16:08:22 +0300

    feat: add hardware models for motherboard, gpu, memory, storage

--- src/models.py
+++ src/models.py
@@ -7,14 +7,11 @@ class Metric:
     value:  str
     labels: dict
 
-
-
 @dataclass
 class FanHardware:
     index:      int
     control:    float | None = None
     rpm:        float | None = None
-
 
 
 # --------- CPU --------- #
@@ -26,7 +23,6 @@ class CpuCore:
     voltage:            float | None = None
     power:              float | None = None
     load:               float | None = None
-
 
 @dataclass
 class CpuHardware:
@@ -43,11 +39,86 @@ class CpuHardware:
     cores:                      dict[int, CpuCore] = field(default_factory=dict)
 
 
+# --------- Motherboard --------- #
+@dataclass
+class MotherboardHardware:
+    fans:           dict[str, FanHardware] = field(default_factory=dict)
+    temperature:    dict[str, float] = field(default_factory=dict)
+    voltage:        dict[str, float] = field(default_factory=dict)
 
 
+# --------- GPU (NVIDIA) --------- #
+@dataclass
+class GpuHardware:
+    core_temp:                  float | None = None
+    memory_junction_temp:       float | None = None
+
+    core_load:                  float | None = None
+    memory_load:                float | None = None
+    memory_controller_load:     float | None = None
+    d3d_3d_load:                float | None = None
+
+    power:                      float | None = None
+
+    core_clock:                 float | None = None
+    memory_clock:               float | None = None
+
+    memory_used:                float | None = None
+    memory_free:                float | None = None
+    memory_total:               float | None = None
+    d3d_dedicated_memory_used:  float | None = None
+    d3d_shared_memory_used:     float | None = None
+
+    pcie_rx:                    float | None = None
+    pcie_tx:                    float | None = None
+
+    fans:                       dict[int, FanHardware] = field(default_factory=dict)
+
+
+# --------- Memory --------- #
+@dataclass
+class MemoryHardware:
+    load:               float | None = None
+    used:               float | None = None
+    available:          float | None = None
+
+    virtual_load:       float | None = None
+    virtual_used:       float | None = None
+    virtual_available:  float | None = None
+
+    dimms:              dict[str, float] = field(default_factory=dict)
+
+
+# --------- Storage --------- #
+@dataclass
+class StorageDevice:
+    name:               str
+    temperature:        float | None = None
+
+    read_activity:      float | None = None
+    write_activity:     float | None = None
+    total_activity:     float | None = None
+
+    read_rate:          float | None = None
+    write_rate:         float | None = None
+
+    used_space:         float | None = None
+    free_space:         float | None = None
+
+    life:               float | None = None
+    percentage_used:    float | None = None
+    available_spare:    float | None = None
+
+@dataclass
+class StorageHardware:
+    devices:     dict[str, StorageDevice] = field(default_factory=dict)
 
 
 
 @dataclass
 class HardwareMetrics:
-    cpu: CpuHardware = field(default_factory=CpuHardware)
+    motherboard:    MotherboardHardware = field(default_factory=MotherboardHardware)
+    cpu:            CpuHardware = field(default_factory=CpuHardware)
+    gpu:            GpuHardware = field(default_factory=GpuHardware)
+    memory:         MemoryHardware = field(default_factory=MemoryHardware)
+    storage:        StorageHardware = field(default_factory=StorageHardware)
